904e84ad06be7489de738e321f18da7955020325
Sourav Prasad <souravprasad731@gmail.com>
2020-05-08 22:54:25 +0530

    columns pickel created,api working

--- API/api.py
+++ API/api.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+import pickle
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+filename = '../breastcancer.pkl'
+with open(filename,'rb') as file:
+    model = pickle.load(file)
+
+filename = '../breastcancercolumns.pkl'
+with open(filename,'rb') as file:
+    columns = pickle.load(file)
 
 @app.route("/")
 def hello():
-    return "Welcome to machine learning model APIs!"
+    return "Welcome to machine learning model API!"
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    if model:
+        try:
+            json_ = request.json
+
+            try:
+                query = pd.get_dummies(pd.DataFrame(json_))
+            except:
+                return jsonify({'error' : 'something went wrong'})
+
+            query = query.reindex(columns=columns, fill_value=0)
+            prediction = model.predict(query)
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+            return jsonify({'error' : 'something went wrong'})
+    else:
+        return jsonify({'error' : 'Model not loaded'})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
