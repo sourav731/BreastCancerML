95f7e79fad90bfedb00b06a77117ddc970b36875
Sourav Prasad <souravprasad731@gmail.com>
2020-05-08 13:53:15 +0530

    visualization and univariate analysis

--- breastcancer.py
+++ breastcancer.py
@@ -41,3 +41,96 @@ df = df.drop(['Unnamed: 32'],axis=1)
 
 # %%
 # df['diagnosis'].value_counts()
+
+# # VISUALISATIAN
+# 
+
+# %%
+def boxplotFloatDataTypes(columnName):
+    plt.figure(figsize=(10,10))
+    sns.boxplot(x='diagnosis',y=columnName,data=df)
+    plt.show()
+
+
+# %%
+# for col in df.columns:
+#     if col != 'diagnosis' and col != 'id':
+#         boxplotFloatDataTypes(col)
+#     else :
+#         continue
+
+
+# %%
+def linePlot(columnName):
+    plt.figure(figsize=(25,5))
+    plt.plot(df[columnName],marker='o',label=columnName)
+    plt.legend()
+    meanArray = []
+    xAxis = []
+    for i in range(1,570):
+        meanArray.append(np.mean(df[columnName]))
+        xAxis.append(i)
+
+    plt.plot(xAxis,meanArray,color='red',linestyle='dashed',label='mean')
+    plt.legend()
+
+
+# %%
+# for col in df.columns:
+#     if col != 'diagnosis' and col != 'id':
+#         linePlot(col)
+#     else :
+#         continue
+
+
+# %%
+# df.skew()
+
+
+# %%
+# df['concavity_se'].skew()
+
+
+# %%
+# df['area_se'].skew()
+
+
+# %%
+# df['concavity_se'].plot()
+
+
+# %%
+# df[df['concavity_se'] > 0.20]
+
+
+# %%
+# df['area_se'].plot()
+
+
+# %%
+# df[df['area_se'] > 200]
+
+
+# %%
+df['area_se'] = np.log(df['area_se'])
+
+
+# %%
+# df['concavity_se']
+
+
+# %%
+df['concavity_se'] = np.log(df['concavity_se'])
+
+
+# %%
+df['concavity_se'] = df['concavity_se'].replace([np.inf, -np.inf], 0)
+
+
+# %%
+# df['concavity_se'].skew()
+
+
+# %%
+# df['radius_se'].skew()
+# df['radius_se'] = np.log(df['radius_se'])
