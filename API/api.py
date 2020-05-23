436d126bac5c7c41db20ccd6cf614623dc3e332e
Sourav Prasad <souravprasad731@gmail.com>
2020-05-23 18:41:32 +0530

    api connected

--- API/api.py
+++ API/api.py
@@ -1,8 +1,11 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,make_response
 import pickle
 import numpy as np
 import pandas as pd
+from flask_cors import CORS
 
+app = Flask(__name__)
+cors = CORS(app)
 app = Flask(__name__)
 
 filename = '../breastcancer.pkl'
@@ -13,30 +16,42 @@ filename = '../breastcancercolumns.pkl'
 with open(filename,'rb') as file:
     columns = pickle.load(file)
 
+def build_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+def build_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 @app.route("/")
 def hello():
     return "Welcome to machine learning model API!"
 
-@app.route("/predict",methods=['POST'])
+@app.route("/predict",methods=['OPTIONS','POST'])
 def predict():
-    if model:
-        try:
-            json_ = request.json
+	if request.method == 'OPTIONS': 
+        	return build_preflight_response()
 
-            try:
-                query = pd.get_dummies(pd.DataFrame(json_))
-            except:
-                return jsonify({'error' : 'something went wrong'})
+	elif request.method == 'POST':
+    		if model:
+        		try:
+            			json_ = request.json
 
-            query = query.reindex(columns=columns, fill_value=0)
-            prediction = model.predict(query)
+            			try:
+                			query = pd.get_dummies(pd.DataFrame(json_))
+            			except:
+                			return jsonify({'error' : 'something went wrong'})
 
-            return jsonify({'prediction': str(prediction)})
+            			query = query.reindex(columns=columns, fill_value=0)
+            			prediction = model.predict(query)
 
-        except:
-            return jsonify({'error' : 'something went wrong'})
-    else:
-        return jsonify({'error' : 'Model not loaded'})
+            			return build_actual_response(jsonify({'prediction': str(prediction)}))
+
+        		except:
+            			return jsonify({'error' : 'something went wrong'})
 
 
 if __name__ == '__main__':
